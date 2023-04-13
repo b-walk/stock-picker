ba8840da8fd159dd1df9c9b299d2bb918ca27027
Blake <blameswalker@gmail.com>
2023-04-12 17:10:41 -0700

    Add variable 'sorted_prices' containing the stock prices array sorted from low to high

--- app.rb
+++ app.rb
@@ -1,1 +1,13 @@
+def stock_picker(stock_prices)
+    sorted_prices = stock_prices.sort
+    
+end
 
+# STEPS:
+# Sort stock price array into a new array, from lowest to highest values
+# Find the lowest and highest values
+# Determine if the lowest value comes before the highest value
+
+# If it does, return their indeces (days) from the original array
+# If it does not:
+# Find a new highest value or new lowest value, depending on which one would yield the higher profit and repeat the earlier process
