af68192335acd6e975d3d0f8b4f7da9e89dd1825
Blake <blameswalker@gmail.com>
2023-04-14 02:53:13 -0700

    Implement stock_picker method

--- app.rb
+++ app.rb
@@ -1,14 +1,35 @@
-def stock_picker(stock_prices)
-    sorted_prices = stock_prices.sort
-    lowest_price = sorted_price[0]
-    highest_price = sorted_price[-1]
+require 'pry-byebug'
+
+def stock_picker(prices)
+    ideal_buy_price = prices.min
+    ideal_sell_price = prices.max
+
+    if prices.index(ideal_buy_price) < prices.index(ideal_sell_price)
+        return [prices.index(ideal_buy_price), prices.index(ideal_sell_price)]
+    end
+
+    prices_and_days = []
+
+    prices.each_with_index do |price, day|
+        prices_and_days[day] = [day, price]
+    end
+
+    price_and_day_pairs = prices_and_days.combination(2).to_a
+
+    best_profit_pair = (price_and_day_pairs.max_by {|pair| pair[1][1] - pair[0][1]})
+    best_days = Array.new(2)
+    best_days.push(best_profit_pair[0][0], best_profit_pair[1][0])
+    
+    best_days
 end
 
-# STEPS:
-# Sort stock price array into a new array, from lowest to highest values
-# Find the lowest and highest values
-# Determine if the lowest value comes before the highest value
+prices = [16, 17, 8, 3, 14, 3, 90, 85, 92, 3, 89]
+p stock_picker(prices)
 
-# If it does, return their indeces (days) from the original array
-# If it does not:
-# Find a new highest value or new lowest value, depending on which one would yield the higher profit and repeat the earlier process
+# OBJECTIVE:
+# Given an array of stock prices, determine the best day to buy and the best day to sell
+
+# STEPS:
+# Find the highest and lowest values
+# If the index of the lowest value is less than the index of the highest value, return their indeces
+# If not, find the next highest value and repeat the test
